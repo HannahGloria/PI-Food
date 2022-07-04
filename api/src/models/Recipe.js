f002b9080dabcc6841e738071ba038fb35441cd6
Hannah Gloria <HannahGloria.eleonor.ragl@gmail.com>
2022-07-03 22:48:13 -0500

    models

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -1,12 +1,38 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
+//Get Recipe Information (Api)
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    summary: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    healthScore: {
+      type: DataTypes.FLOAT,
+    },
+    analyzedInstructions: {
+      type: DataTypes.TEXT,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+    createdInDb:{
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    },
+  },{ timestamps: false});
 };
